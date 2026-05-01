b1e33929da6e891717baf59812e00c27c5083846
kirillkruglikov <kirillkruglikov@gmail.com>
2026-05-01 17:20:58 +0300

    update nvim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.clipboard = "unnamedplus"
-vim.opt.winbar = "%=%m %f"
-vim.o.background = "light"
+
+vim.g.lazyvim_python_lsp = "basedpyright"
+-- vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "ty"
+-- vim.g.lazyvim_python_lsp = "pylsp"
+
+vim.g.lazyvim_blink_main = true
+vim.opt.updatetime = 250
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
