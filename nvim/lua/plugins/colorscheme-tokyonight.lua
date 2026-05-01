a57a7c89b8116fa664650fbeaaedc5ff6f4777f7
kirillkruglikov <kirillkruglikov@gmail.com>
2026-05-01 17:21:24 +0300

    add tokyonight colorscheme

---
+++ nvim/lua/plugins/colorscheme-tokyonight.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      light_style = "day",
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+    },
+  },
+}
