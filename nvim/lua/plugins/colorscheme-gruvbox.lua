1c19e988509bb358c2b94c78eac1fe2381ce41b9
kirillkruglikov <kirillkruglikov@gmail.com>
2026-05-01 17:21:24 +0300

    add gruvbox colorscheme

---
+++ nvim/lua/plugins/colorscheme-gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    enabled = false,
+    priority = 1000,
+    opts = {
+      contrast = "",
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+    },
+  },
+}
