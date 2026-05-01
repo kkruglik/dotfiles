efee52f6138f56c7e1f77db92a9442bf10a0afa5
kirillkruglikov <kirillkruglikov@gmail.com>
2026-05-01 17:20:47 +0300

    update nvim autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,24 +1,8 @@
--- Final optimized version - minimal overhead
-local theme_cache = { hour = nil, theme = nil }
-
-local function update_time_theme()
-  local hour = tonumber(os.date("%H"))
-
-  if theme_cache.hour == hour then
-    return
-  end
-  theme_cache.hour = hour
-
-  local target = (hour >= 7 and hour < 18) and "light" or "dark"
-
-  if theme_cache.theme ~= target then
-    theme_cache.theme = target
-    vim.schedule(function()
-      vim.o.background = target
-      vim.cmd("colorscheme tokyonight-" .. (target == "light" and "day" or "storm"))
-    end)
-  end
-end
-
-vim.fn.timer_start(3600000, update_time_theme, { ["repeat"] = -1 })
-vim.api.nvim_create_autocmd("VimEnter", { callback = update_time_theme })
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
